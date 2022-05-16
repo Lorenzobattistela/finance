ddaf521377c72b028a8eccfb3d3195d9d8d5e5bc
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-05-16 16:53:34 -0300

    showing balance

--- report.py
+++ report.py
@@ -69,20 +69,30 @@ def construct_html():
 
         if 'quote' in label:
             quote_index = label.index('quote')
-        
+
         html += f'<h2>{typ}</h2><div class="tables">'
+
         for i in range(len(type_data)):
             html += '<table>'
+
             for j in range(len(label)):
                 html += f'<tr><th scope="col">{label[j]}</th></tr>'
                 html += f'<tr><td>{type_data[i][j]} </td></tr>'
 
             if quote_index != None:
                 actual_price = get_actual_price(type_data[i][quote_index])
+             
                 html += '<tr><th scope="col">Actual_price</th></tr>'
                 html += f'<tr><td >{actual_price}</td></tr>'
-           
 
+                if actual_price != 'Esse símbolo não foi encontrado.':
+                    price_index = label.index('buy_price')
+                    quantity_index = label.index('quantity')
+
+                    balance = calculate_balance(type_data[i][price_index], actual_price, type_data[i][quantity_index])
+
+                    html += '<tr><th scope="col">Balance</th></tr>'
+                    html += f'<tr><td >{balance}</td></tr>'
             html += '</table>'
 
 
@@ -92,13 +102,24 @@ def construct_html():
     doc_html = insert_in_html_template(html)
     return doc_html
 
+def calculate_balance(buy_price, actual_price, quantity):
+    buy_price = float(buy_price)
+    actual_price = float(actual_price)
+    quantity = float(quantity)
+    balance = (actual_price - buy_price) * quantity
+    return round(balance, 2)
 
 def get_actual_price(quote):
     lower_quote = quote.lower()
     api_key = os.getenv('API_KEY')
     response = requests.get(f'https://api.hgbrasil.com/finance/stock_price?key={api_key}&symbol={lower_quote}')
-    json = response.json()
-    actual_price = json['results'][quote]['price']
+    response_json = response.json()
+    quote_info = response_json['results'][quote]
+
+    try:
+        actual_price = quote_info['price']
+    except:
+        return 'Esse símbolo não foi encontrado.'   
     return actual_price
 
 def insert_in_html_template(html):
